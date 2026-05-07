48e5751fdbffa2a1e90ce8fbbe61a6c358c40f66
Daveberry <84567826+daveberrys@users.noreply.github.com>
2026-05-08 04:28:33 +0800

    fix: forgotten `path` arg in `run`

--- src/commands/run.rs
+++ src/commands/run.rs
@@ -4,25 +4,44 @@ use std::error::Error;
 use colored::Colorize;
 use super::venv;
 
-pub fn run(_path: &str) -> Result<(), Box<dyn Error>> {
+pub fn run(path: &str) -> Result<(), Box<dyn Error>> {
     if !Path::new(".venv").is_dir() {
         venv::run("")?;
     }
-    start_app()?;
 
+    if path.is_empty() {
+        start_app(None)?;
+    } else {
+        start_app(Some(path))?;
+    }
+    
     Ok(())
 }
 
-fn start_app() -> Result<(), Box<dyn Error>> {
+fn start_app(path: Option<&str>) -> Result<(), Box<dyn Error>> {
     println!("{}", "Starting application...".bright_black());
-
+    
+    let python_path;
+    match path {
+        None => {
+            if cfg!(target_os = "windows") {
+                python_path = "src\\main.py";
+            } else {
+                python_path = "src/main.py";
+            }
+        }
+        Some(v) => {
+            python_path = v;
+        }
+    }
+    
     if cfg!(target_os = "windows") {
         let mut cmd = Command::new(".venv\\Scripts\\python.exe");
-        cmd.arg("src\\main.py");
+        cmd.arg(python_path);
         cmd.status()?;
     } else {
         let mut cmd = Command::new(".venv/bin/python");
-        cmd.arg("src/main.py");
+        cmd.arg(python_path);
         cmd.status()?;
     }
 
